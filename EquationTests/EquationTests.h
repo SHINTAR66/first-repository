dde7ceb273c6ccbe5c67f7885854393dbb05277e
Shintaro Ohki <q9531013@eng.kitakyu-u.ac.jp>
2012-12-13 14:40:02 +0900

    Initial Commit

---
+++ EquationTests/EquationTests.h
@@ -0,0 +1,13 @@
+//
+//  EquationTests.h
+//  EquationTests
+//
+//  Created by 大木　進太郎 on 12/12/13.
+//  Copyright (c) 2012年 admin. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface EquationTests : SenTestCase
+
+@end
